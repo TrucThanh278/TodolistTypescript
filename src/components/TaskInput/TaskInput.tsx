325a633003c3060156b43884025abf684b57f5ad
Truc Nguyen Thanh <trucnguyenthanh56@gmail.com>
2024-09-14 02:15:45 +0700

    chore: add readme file

--- src/components/TaskInput/TaskInput.tsx
+++ src/components/TaskInput/TaskInput.tsx
@@ -1,48 +1,50 @@
-import { useState } from 'react'
-import styles from './taskInput.module.scss'
-import { Todo } from '../../@types/todo.type'
+import { useState } from "react";
+import styles from "./taskInput.module.scss";
+import { Todo } from "../../@types/todo.type";
 
 interface taskInputProps {
-  addTodo: (name:string) => void
-  currentTodo: Todo|null
-  editTodo: (name: string) => void
-  finishEditTodo: () => void
+	addTodo: (name: string) => void;
+	currentTodo: Todo | null;
+	editTodo: (name: string) => void;
+	finishEditTodo: () => void;
 }
 
 export default function TaskInput(props: taskInputProps) {
-  const {addTodo, currentTodo, editTodo, finishEditTodo} = props
-  const [name, setName] = useState<string>("")
+	const { addTodo, currentTodo, editTodo, finishEditTodo } = props;
+	const [name, setName] = useState<string>("");
 
-  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    if (currentTodo) {
-      finishEditTodo()
-      if(name) setName("")
-    }else{
-      addTodo(name)
-      setName("")
-    }
-  }
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (currentTodo) {
+			finishEditTodo();
+			if (name) setName("");
+		} else {
+			addTodo(name);
+			setName("");
+		}
+	};
 
-  const onChangeInput = (event:React.ChangeEvent<HTMLInputElement>) => {
-    const {value} = event.target
-    if(currentTodo ){
-      editTodo(value)
-    }else{
-      setName(value)
-    }
-  }
+	const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		if (currentTodo) {
+			editTodo(value);
+		} else {
+			setName(value);
+		}
+	};
 
-  return (
-    <div>
-        <h1 className={styles.title}>To do list TypeScript</h1>
-        <form action="" className={styles.form} onSubmit={handleSubmit}>
-            <input type="text" placeholder='caption goes here' 
-                    value={currentTodo ? currentTodo.name : name} 
-                    onChange={ onChangeInput }
-                    />
-            <button type='submit'>{ currentTodo ? '✔' : '+' }</button>
-        </form>
-    </div>
-  )
+	return (
+		<div>
+			<h1 className={styles.title}>To do List TypeScript</h1>
+			<form action="" className={styles.form} onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="caption goes here"
+					value={currentTodo ? currentTodo.name : name}
+					onChange={onChangeInput}
+				/>
+				<button type="submit">{currentTodo ? "✔" : "+"}</button>
+			</form>
+		</div>
+	);
 }
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,36 +1,60 @@
+import { Todo } from "../../@types/todo.type";
+import styles from "./taskList.module.scss";
 
-import { Todo } from '../../@types/todo.type';
-import styles from './taskList.module.scss'
-
-interface TaskListProps{
-  doneTaskList?: boolean;
-  todos: Todo[];
-  handleDoneTodo: (id: string, status: boolean) => void;
-  startEditTodo: (id: string) => void
-  deleteTodo: (id: string) => void
+interface TaskListProps {
+	doneTaskList?: boolean;
+	todos: Todo[];
+	handleDoneTodo: (id: string, status: boolean) => void;
+	startEditTodo: (id: string) => void;
+	deleteTodo: (id: string) => void;
 }
 
 export default function TaskList(props: TaskListProps) {
-  const {doneTaskList, todos, handleDoneTodo, startEditTodo, deleteTodo} = props
+	const { doneTaskList, todos, handleDoneTodo, startEditTodo, deleteTodo } =
+		props;
 
-  return (
-    <div className='mb-2'>
-        <h2 className={styles.tasksTitle}>
-          {doneTaskList ? 'Hoàn thành' : 'Chưa hoàn thành'}
-        </h2>
-        <div className={styles.tasks}>
-
-          {todos.map((todo) =>
-            <div className={styles.taskItem} key={todo.id}>
-              <input type="checkbox" className={`${styles.taskCheckbox} ${todo.done ? styles.taskNameDone : ''}` } checked={todo.done} onChange={(e) => handleDoneTodo(todo.id, e.target.checked)}/>
-              <span className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ''}`}>{todo.name}</span>
-              <div className={styles.taskActions}>
-                  <button className={styles.taskBtn} onClick={() => startEditTodo(todo.id)}>🖋</button>
-                  <button className={styles.taskBtn} onClick={() => deleteTodo(todo.id)}>🗑</button>
-              </div>
-          </div>
-          )}
-        </div>
-    </div>
-  )
+	return (
+		<div className="mb-2">
+			<h3 className={styles.tasksTitle}>
+				{doneTaskList ? "Completed tasks" : "Pending tasks"}
+			</h3>
+			<div className={styles.tasks}>
+				{todos.map((todo) => (
+					<div className={styles.taskItem} key={todo.id}>
+						<input
+							type="checkbox"
+							className={`${styles.taskCheckbox} ${
+								todo.done ? styles.taskNameDone : ""
+							}`}
+							checked={todo.done}
+							onChange={(e) =>
+								handleDoneTodo(todo.id, e.target.checked)
+							}
+						/>
+						<span
+							className={`${styles.taskName} ${
+								todo.done ? styles.taskNameDone : ""
+							}`}
+						>
+							{todo.name}
+						</span>
+						<div className={styles.taskActions}>
+							<button
+								className={styles.taskBtn}
+								onClick={() => startEditTodo(todo.id)}
+							>
+								🖋
+							</button>
+							<button
+								className={styles.taskBtn}
+								onClick={() => deleteTodo(todo.id)}
+							>
+								🗑
+							</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
